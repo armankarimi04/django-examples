2be6fb5b3178e181508c79ae688ccbd30a6de1a1
armk7 <armankarimy7@gmail.com>
2026-02-10 16:13:38 +0330

    Wagtail CMS base project

---
+++ trying-out-wagtail-cms/mysite/mysite/settings/dev.py
@@ -0,0 +1,18 @@
+from .base import *
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = True
+
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = "django-insecure-iml-#yd5mj64f))m&itq6s7f@r2%ny883$ug6^-$&iwwx94ng#"
+
+# SECURITY WARNING: define the correct hosts in production!
+ALLOWED_HOSTS = ["*"]
+
+EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+
+
+try:
+    from .local import *
+except ImportError:
+    pass

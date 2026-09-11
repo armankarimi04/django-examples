23148997ad75500b0346f538fad42c80d022e762
armk7 <armankarimy7@gmail.com>
2026-09-11 18:17:39 +0430

    Project Update: Django + Alpine.js, HTMX (Vite integration)

--- django-vite-htmx-alpinejs-tailwindcss/backend/main/urls.py
+++ django-vite-htmx-alpinejs-tailwindcss/backend/main/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
 from . import views
-
+app_name = "main"
 urlpatterns = [
+    path('htmx-test/', views.htmx_test, name='htmx_test'),
+    path('test-page/', views.test_page),
     path('', views.index, name='index')   
 ]
--- django-vite-htmx-alpinejs-tailwindcss/backend/main/views.py
+++ django-vite-htmx-alpinejs-tailwindcss/backend/main/views.py
@@ -1,6 +1,15 @@
+from django.http.request import HttpRequest
+from django.http.response import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
 
-def index(request):
+def index(request: HttpRequest) -> HttpResponse:
     return render(request, "main/index.html", {})
+
+
+def htmx_test(request: HttpRequest) -> HttpResponse:
+    return HttpResponse("<div>Hello from django server.</div>")
+
+def test_page(request: HttpRequest) -> HttpResponse:
+    return render(request, "main/test_page.html", {})
